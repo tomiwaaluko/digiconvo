a79cf3a1bc997d0ee8d399f4f477f32a325f3427
Liam Huang <li104704@ucf.edu>
2025-06-28 13:55:08 -0400

    lint error fixes

--- src/components/ImageUploadForm.tsx
+++ src/components/ImageUploadForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 import { api } from "~/trpc/react";
 import ReactMarkdown from "react-markdown";
@@ -106,7 +107,13 @@ export default function ImageUploadForm() {
             className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
           >
             {selectedImage ? (
-                <img src={selectedImage} alt="Selected Screenshot" className="object-contain h-full w-full p-2" />
+                <Image 
+                  src={selectedImage} 
+                  alt="Selected Screenshot" 
+                  width={300}
+                  height={200}
+                  className="object-contain h-full w-full p-2" 
+                />
             ) : (
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                     <UploadIcon />
