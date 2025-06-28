4d8cf80fd8f24701616a8b0c64270a61809d2b17
Liam Huang <li104704@ucf.edu>
2025-06-28 12:18:34 -0400

    fixed sidebars error

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,35 +1,49 @@
+"use client";
+
+import React from "react";
 import { ChatInterface } from "~/app/_components/chat/ChatInterface";
 import { ScenarioSidebar } from "~/app/_components/sidebar/ScenarioSidebar";
 import { EmotionPanel } from "~/app/_components/emotion/EmotionPanel";
 import { MobileNavigation } from "~/app/_components/navigation/MobileNavigation";
 import { FloatingActionButton } from "~/app/_components/ui/FloatingActionButton";
+import { useChatStore } from "~/stores/chat";
+import { WelcomeScreen } from "~/app/_components/welcome/WelcomeScreen";
 
 export default function ChatApp() {
+  const { 
+    sidebarOpen, 
+    showEmotionPanel, 
+    messages, 
+    currentScenario,
+    toggleSidebar,
+    toggleEmotionPanel 
+  } = useChatStore();
+
+  // Show welcome screen in the main chat area if no scenario is selected
+  const showWelcomeInChat = !currentScenario;
+
   return (
-    <div className="flex h-screen bg-gray-50 relative">
-      <MobileNavigation />
-      
-      <div className="hidden lg:block">
-        <ScenarioSidebar />
-      </div>
-      
-      <div className="lg:hidden">
-        <ScenarioSidebar />
-      </div>
-      
+    <div className="flex h-screen bg-gray-50">
+      {/* Scenario Sidebar */}
+      <ScenarioSidebar />
+
+      {/* Main Chat Area */}
       <div className="flex-1 flex flex-col min-w-0">
-        <ChatInterface />
+        {showWelcomeInChat ? <WelcomeScreen /> : <ChatInterface />}
       </div>
-      
-      <div className="hidden xl:block">
-        <EmotionPanel />
+
+      {/* Emotion Panel */}
+      <EmotionPanel />
+
+      {/* Mobile Navigation */}
+      <div className="lg:hidden">
+        <MobileNavigation />
       </div>
-      
-      <div className="xl:hidden">
-        <EmotionPanel />
+
+      {/* Floating Action Button for mobile */}
+      <div className="lg:hidden">
+        <FloatingActionButton />
       </div>
-      
-      <FloatingActionButton />
     </div>
   );
 }
